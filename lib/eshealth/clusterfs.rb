c18ad52459e40e94e6b4e30b03465653a8de43ea
Greg Hellings <greg@thesub.net>
2016-10-24 09:02:46 -0600

    Added ability to change percentage to alert about

--- lib/eshealth/clusterfs.rb
+++ lib/eshealth/clusterfs.rb
@@ -4,13 +4,14 @@ require "yaml"
 module Eshealth
   class ClusterFS < Checkfactory
     
-    attr_accessor :url, :configbody, :type, :lastmsg
+    attr_accessor :url, :configbody, :type, :lastmsg, :percentage
     attr_reader :requestfactory
 
     def initialize(options={})
       self.type = "ClusterFS"
       self.url = options[:url] || "http://localhost:9200"
       self.requestfactory = options[:requestfactory] || Eshealth::Requestfactory.build("HttpRequest",options)
+      self.percentage = options[:percentage] || 20
     end
 
     def requestfactory=(requestfactory)
@@ -21,6 +22,7 @@ module Eshealth
     end
 
     def healthstatus
+      percentage = self.percentage.to_f / 100.to_f rescue 0
       begin
         response = self.requestfactory.fetch("_nodes/stats/fs")
       rescue => e
@@ -34,7 +36,7 @@ module Eshealth
       fs["nodes"].each do |node|
         free = node[1]["fs"]["data"][0]["free_in_bytes"].to_f / node[1]["fs"]["data"][0]["total_in_bytes"].to_f rescue 0
         $stdout.puts "Free: #{(free * 100).to_i}%\n"
-        if free < 0.2
+        if free < percentage
           return "red"
         end
       end
